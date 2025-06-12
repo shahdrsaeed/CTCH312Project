3d1444af5dfda7e1c6604dc45bf0ba9fa135ded4
ScientistPaul <ostlundp@uregina.ca>
2025-06-11 19:32:30 -0600

    Add files via upload
    Modification of Shahd's code by Paul to work on Paul's computer

---
+++ InventoryManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject InventoryMenu;
+    private bool menuActivated;
+
+	int numGems = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+         InventoryMenu.SetActive(false);
+         menuActivated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory") && menuActivated)
+        {
+            Time.timeScale = 1;
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        else if (Input.GetButtonDown("Inventory") && !menuActivated) 
+        {
+            Time.timeScale = 0;
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+	numGems += quantity;
+        Debug.Log("itemName = " + itemName + ", quantity = " + numGems + ", itemSprite = " + itemSprite);
+    }
+}
--- Item.cs
+++ Item.cs
@@ -9,7 +9,9 @@ public class Item : MonoBehaviour
     private int quantity;
 
     [SerializeField]
-    private Sprite sprite;
+    private Texture2D spriteTexture;
+
+	private Sprite sprite;
 
     private InventoryManager inventoryManager;
     int numGems = 0;
@@ -21,18 +23,20 @@ public class Item : MonoBehaviour
     {
         thisAudio = GetComponent<AudioSource>();
         inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+    
+	sprite = Sprite.Create(spriteTexture, new Rect(0.0f, 0.0f, spriteTexture.width, spriteTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+
+
     }
 
     private void OnTriggerEnter(Collider collision) 
     {
         if (collision.gameObject.tag == "Player")
         {
-            inventoryManager.AddItem(itemName, quantity, sprite);
-            collision.gameObject.SetActive(false);
-            numGems = numGems + quantity;
-            thisAudio.Play();
-            Destroy(gameObject);
-
+	    inventoryManager.AddItem(itemName, quantity, sprite);
+            //thisAudio.Play();
+		collision.GetComponent<AudioSource>().Play();
+            this.gameObject.SetActive(false);
         }
     }
 }
